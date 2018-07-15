38d12cfbf886b7b7735150e677e91bef444eaa62
NikolaiGrattage <40371541+NikolaiGrattage@users.noreply.github.com>
2018-07-15 15:05:33 +0100

    Calculation for GetLetterGrade RankedGradebook

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -9,6 +10,40 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            int threshold = (int)Math.Ceiling(Students.Count * 0.20);           
+
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+
+            if (grades[threshold - 1] <= averageGrade)
+                return 'A';
+            else if (grades[(threshold * 2) - 1] <= averageGrade)
+                return 'B';
+            else if (grades[(threshold * 3) - 1] <= averageGrade)
+                return 'C';
+            else if (grades[(threshold * 4) - 1] <= averageGrade)
+                return 'D';
+            else
+                return 'F';
+
+            //Same thing without Using Linq?
+            //TO DO: How to make List.Sort() order Descending??
+            //List<double> studentGrades = new List<double>();
+
+            //foreach (Student student in Students)
+            //{
+            //    studentGrades.Add(student.AverageGrade);
+
+            //}
+            //studentGrades.Sort();            
         }
     }
 }
