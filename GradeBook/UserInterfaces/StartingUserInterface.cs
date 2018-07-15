ef143ce202ed8bddf8e1b069bed57111523ded77
NikolaiGrattage <40371541+NikolaiGrattage@users.noreply.github.com>
2018-07-15 16:34:38 +0100

    Amendment

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -55,10 +55,9 @@ namespace GradeBook.UserInterfaces
                     Console.WriteLine(typeErrorMessage);
                     break;
             }
-
-            BaseGradeBook gradeBook = new BaseGradeBook(name);
+            
             Console.WriteLine("Created gradebook {0}.", name);
-            GradeBookUserInterface.CommandLoop(gradeBook);
+            //GradeBookUserInterface.CommandLoop(gradeBook);
         }
 
         public static void LoadCommand(string command)
